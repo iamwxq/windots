4fc69e433f3489c9bc0e5128dc10a26a04c56076
iamwxq <whoeverimf5@gmail.com>
2024-05-28 21:19:55 +0800

    update eslint config file

--- base/eslint.config.mjs
+++ base/eslint.config.mjs
@@ -18,6 +18,7 @@ export default antfu(
   {
     rules: {
       "react/prop-types": "off",
+      "react-refresh/only-export-components": "off",
       "style/jsx-one-expression-per-line": "off",
       "style/jsx-sort-props": [
         "error",
